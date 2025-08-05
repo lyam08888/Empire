96c692668da75742dcb32ac9b3b9c57e5113b197
lyam08888 <lyes.amara8@gmail.com>
2025-08-05 17:59:13 +0200

    Update common.js

--- common.js
+++ common.js
@@ -1,13 +1,13 @@
 const changeAction = type => {
   if (type === "login") {
-    const url = `http://${document.loginForm.universe.value}/index.php`;
+    const url = `http://${document.loginForm.universe.value}/index.html`;
     document.getElementById("loginForm").action = url;
   } else if (type === "getpw") {
-    const url = `http://${document.loginForm.universe.value}/mail.php`;
+    const url = `http://${document.loginForm.universe.value}/mail.html`;
     document.loginForm.action = url;
     document.loginForm.submit();
   } else if (type === "register") {
-    const url = `http://${document.registerForm.universe.value}/register.php`;
+    const url = `http://${document.registerForm.universe.value}/register.html`;
     document.registerForm.action = url;
   }
 };
